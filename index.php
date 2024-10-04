a336463f507a91bf386b15055d2d6730161763d2
daryl <darylb2k11@gmail.com>
2024-10-04 23:00:09 +0800

    first commit and stable

---
+++ index.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--
+  Author: Daryl Bargamento
+  Website Developer: Daryl Bargamento
+  Designer: Jason Jay Gahiton
+-->
+  <head>
+    <title>Jayda</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+    <link rel="icon" href="images/favicon.ico.svg" type="image/x-icon">
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    
+	  <nav class="navbar navbar-expand-lg head-menu" id="ftco-navbar">
+	    <div class="container">
+	      
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"> </span> Menu
+	      </button>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav m-center">
+          <li class="nav-item social-icon" ><a href="#" class="nav-link"><img src="images/logo-blue.png" style="width:150px;height:auto;"></a></li>
+        </ul>
+	        <ul class="navbar-nav m-center">
+            
+	          <li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Services</a></li>	     
+            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Contact Us</a></li>
+            <li class="nav-item social-icon"> <a href="#" class="social-icon"><span class="" ><img src="images/facebook.png" alt="" style="width:30px; height:30px;"/></span></a></li>
+            <li class="nav-item social-icon"> <a href="#" class=""><span class=""><img src="images/instagram.png" alt="" style="width:30px; height:30px;"/></span></a></li>
+            <li class="nav-item callnumber"><a href="tel:0247115093" class="callnumber">02 4711 5093</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    
+    <div class="hero-wrap ftco-degree-bg ftco-animate">
+      <div class="overlay"></div>
+          <div class="container">
+            <div class="row no-gutters slider-text justify-content-start align-items-center justify-content-center">
+              <div class="col-lg-8 ftco-animate">
+                
+                <div class="text w-100 text-center mb-md-5 pb-md-5 py-7">
+                  <img src="images/icon.png" alt="" class="py-4" style="width:327px; height:auto ;">
+                  <h1 class=" text-white mx-md-n1 ">Business Solutions</h1>
+                  <h3  class="text-white poppins xl-size pb-5">One Stop Shop/Destination For Your Business Needs.</h3>
+                  <p class="text-white poppins " >Get your business online today Jayda</p>
+                  <p class="text-white poppins">Jayda & Co A Multitude Of Professional Business Services That Centers On.</p>
+                  <p class="text-white poppins"> Increasing The Value of Our Clients.</p>
+                      <p class="pt-5 mt-5 downarrowmobl"><a href="#"><span class="mb-md-5 pb-md-5 py-5"><img src="images/downarrow.png" style="width:30px;height:30px;" /></img></span></a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+    
+    <section class="ftco-section ftco-animate testimony-section bg-services">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center heading-section ftco-animate ">
+          <h2 class="mb-3 poppins">Our Services</h2>
+          </div>
+        </div>
+        <div class="row ftco-animate">
+          <div class="col-md-12">
+            <div class="carousel-testimony owl-carousel ftco-owl">
+              <div class="item bg-services">
+                <div class="testimony-wrap rounded text-center py-0 pb-5 bg-services ">
+                  <div class="user-img mb-2" style="background-image: url('images/bookkeeping.png')">
+                  </div>
+                  <div class="">
+                  <h3 class="heading py-3 servicesheadsection">BUSINESS RECORD & TAX SOLUTIONS</h3>
+                  <div class="flex justify-content-center">
+                    <ul>
+                      <li class="list-services">Bookkeeping</li>
+                      <li class="list-services mb-2">Accounting</li>
+                    </ul>
+                    
+                  </div>
+                  <a href="#" class="pl-3"><span class="learnmore "> Learn More </span></a>
+                  </div>
+                </div>
+              </div>
+              <div class="item col-md-12">
+                <div class="testimony-wrap rounded text-center py-0 pb-5 bg-services">
+                  <div class="user-img mb-2" style="background-image: url('images/business-digita-setup.png');">
+                  </div>
+                  
+                  
+                  <div class="flex flex-column justify-content-center">
+                  <h3 class="heading pt-3 servicesheadsection">BUSINESS ONLINE-DIGITAL <br  class="setupmobile" />SET-UP
+                  <span><br class="maintainmobile" />& MAINTENANCE </span>
+                  </h3>
+                  <div class="flex justify-content-center">
+                    <ul>
+                      <li class="list-services">Graphic Design</li>
+                      <li class="list-services">Web Design</li>
+                      <li class="list-services mb-0">Social Media</li>
+                    </ul>
+                  </div>
+                    <a href="#"><span class="learnmore"> Learn More </span></a>
+                  </div>
+                </div>
+              </div>
+
+            
+              <div class="item col-md-12">
+                <div class="testimony-wrap rounded text-center py-0 pb-5 bg-services ">
+                  <div class="user-img mb-2" style="background-image: url('images/digital-printing.png')">
+                  </div>
+                  <div class="">
+                  <h3 class="heading py-3 servicesheadsection">SIGNAGE BUSINESS SOLUTIONS</h3>
+                  <div class="flex flex-column justify-content-center mb-4">
+                    <p class="textservices line-h">All types of signage including</p>
+                    <p class="textservices line-h">digital signage</p>
+                    <p class="textservices line-h">Link:</p>
+                    <p class="textservices line-h">Signarama Penrith store</p>
+                  </div>
+                    <a href="#" ><span class="learnmore"> Learn More </span></a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="item col-md-12">
+                <div class="testimony-wrap rounded text-center py-0 pb-0 bg-services ">
+                  <div class="user-img mb-2" style="background-image: url('images/uniform-sulotions.png')">
+                  </div>
+                  <div class="">
+                  <h3 class="heading py-3 servicesheadsection">UNIFORM SOLUTIONS</h3>
+                  <div class="flex flex-column justify-content-center mb-4">
+                    <p class="textservices line-h">All types of signage including</p>
+                    <p class="textservices line-h">digital printing to uniforms</p>
+                    <p class="textservices line-h">Link: Fully promoted Penrith store</p>
+                    <p class="textservices line-h">- Opening soon</p>
+                  </div>
+                    <a href="#"><span class="learnmore"> Learn More </span></a>
+                  </div>
+                </div>
+              </div>
+              <div class="item col-md-12">
+                <div class="testimony-wrap rounded text-center py-0 pb-5 bg-services ">
+                  <div class="user-img mb-2" style="background-image: url('images/promotial-products-sulotion.png')">
+                  </div>
+                  <div class="">
+                  <h3 class="heading py-3 servicesheadsection">PROMOTIONAL PRODUCTS SOLUTIONS</h3>
+                  <p class="textservices line-h">Link: Fully promoted Penrith store</p>
+                  
+                    <a href="#"><span class="learnmore"> Learn More </span></a>
+                  </div>
+                </div>
+              </div>
+              <div class="item col-md-12">
+                <div class="testimony-wrap rounded text-center py-0 pb-5 bg-services ">
+                  <div class="user-img mb-2" style="background-image: url('images/otherbusinesssolutions.png')">
+                  </div>
+                  <div class="">
+                  <h3 class="heading py-3 servicesheadsection">OTHER BUSINESS SOLUTIONS</h3>
+                  
+                  <div class="mt-4 pt-1"></div>
+                    <a href="#"><span class="learnmore"> Learn More </span></a>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  <!-- end first section services-->
+
+<!--Lets talk -->
+    
+<section class="ftco-section bg-services ftco-animate">
+    <div class="container">
+      <div class="row justify-content-center ">
+        <div class="col-md-7 text-center heading-sectionftco-animate">
+        <h2 class="mb-3 poppins letstalk">Lets Talk</h2>       
+        </div>
+      </div>
+      <div class="row md ftco-animate">
+        
+        <div class="col-md-6">
+          <div class="services services-2 w-100">            	
+            <div class="">
+            <div class="flex flex-column  w-100 ">
+            <h3 class="heading py-3 servicesheadsection">Contact Us </h3>
+              <div class="flex fllex-row ">
+                    <div class="flex-fill ">
+                      <p class="textservices">Phone:</p>
+                      <p class="textservices">Email:</p>
+                      <p class="textservices">Address:</p>
+                    </div>
+                    <div class="flex-fill ">
+                      <p class="textservices">02 4711 5093</p>
+                      <p class="textservices">bizsolutions@jayda.com.au</p>
+                      <p class="textservices">6/29 Abel Street, Jamisontown, <span><br /> NSW 2750, Australia</span></p>
+                    </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="services services-2 w-100 ">            	
+            <div class="w-100 ">
+            <div class="flex flex-column w-100 ">
+            <h3 class="heading py-3 align-self-center servicesheadsection">&nbsp; &nbsp;Business Hours </h3>
+                <div class="flex flex-row">
+                  <div class="flex-fill  w-25">
+                      <p class="textservices">&nbsp;</p>
+                      
+                  </div>
+                  <div class="flex-fill w-25">
+                        <p class="textservices">Mon</p>
+                        <p class="textservices">Tue</p>
+                        <p class="textservices">Wed</p>
+                        <p class="textservices">Thu</p>
+                        <p class="textservices">Fri</p>
+                        <p class="textservices">Sat</p>
+                  </div>
+                  <div class="flex-fill w-50">
+                      <p class="textservices">9:00am - 4:30pm</p>
+                      <p class="textservices">9:00am - 4:30pm</p>
+                      <p class="textservices">9:00am - 4:30pm</p>
+                      <p class="textservices">9:00am - 4:30pm</p>
+                      <p class="textservices">9:00am - 4:30pm</p>
+                      <p class="textservices">9:00am - 4:30pm</p>
+                  </div>
+                </div>
+            </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </section> 
+
+<!-- end lets talk section-->
+
+<!-- start form with captcha -->
+
+ 
+<section class="ftco-section bg-services ftco-animate" style="padding-top:0px !important;">
+    <div class="container">
+      <div class="row justify-content-center ">
+        <div class="col-md-10 text-center heading-section ftco-animate">
+            <forction="#">
+              <div class="w-100">
+                <input type="text" name="name" id="" placeholder="Name" class="inputform mr-2 inputmax">
+                <input type="text" name="phone" id="" placeholder="Phone No." class="inputform mr-2 inputmax">
+                <input type="email" name="email" id="" placeholder="Your Address" class="inputform inputmax">
+              </div>
+              <div class="clearfix">&nbsp;</div>
+              <div class="">
+                <textarea name="faq" id="faq" class="inputform w-70 inputmax" rows="4">Ask us any questions..  
+                </textarea>
+              </div>
+              <div class="clearfix">&nbsp;</div>
+              <div class="w-25  mb-4" style="border:1px solid #04a3b9; margin:0 auto;">
+                <input type="checkbox" name="captcha"><span> I'm Not a robot</span>
+              </div>
+              <input type="submit" name="submit" id="" value="Submit" class="submit">
+            </form>
+        </div>
+      </div>
+      
+    </div>
+  </section>
+
+<!-- end form captcha-->
+
+
+    
+
+    <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+       
+        <div class="row p-2">
+          <div class="col-md-6">
+              <p class="text-white poppins text-center">
+                &copy; <?= date('Y');?> Jayda & Co All rights reserved </a>
+              </p>
+          </div>
+          <div class="col-md-6 text-center">
+                <a href="#"><span class="mr-4"><img src="images/fb-white.png" alt="" style="width:30px; height:30px;"/></span></a>
+                <a href="#"><span class=""><img src="images/white-instagram.png" alt="" style="width:30px; height:30px;"/></span></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
